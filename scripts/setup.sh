1d167caa35b7fcd7a17a3d1a3889df653d84d8dc
Sean Kirmani <sean@kirmani.io>
2017-10-06 15:05:45 -0500

    Add catkin workspace with relevant ROS packages (#24)
    * Add catkin workspace with relevant ROS packages
    
    * add ros build/ and devel/ to gitignore
    
    * fully automate the setup script
    
    * use binary of ardrone_autonomy in favor of building from source

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,8 +6,33 @@
 # Distributed under terms of the MIT license.
 #
 
+PROJECT_DIR=`pwd -P`
+
 # Set-up githooks.
 git config core.hooksPath .githooks
 
+# Initialize and update submodules.
+git submodule update --init --recursive
+
+# Link catkin workspace.
+unlink ~/catkin_ws
+ln -s $PROJECT_DIR/../catkin_ws ~/catkin_ws
+
 # Install ROS.
-echo "TODO(kirmani): Add script to set-up ROS"
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-get update
+sudo apt-get -y install ros-indigo-desktop-full
+sudo rosdep init
+rosdep update
+echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt-get -y install python-rosinstall
+
+# Install ardrone_autonomy.
+sudo apt-get -y install ros-indigo-ardrone-autonomy
+
+# Build catkin workspace.
+cd ~/catkin_ws
+source devel/setup.bash
+catkin_make
