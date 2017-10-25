bfd5026ea177262c75a864344e0fc6ba14cbac1b
Sean Kirmani <sean@kirmani.io>
2017-10-25 09:35:31 -0500

    can successfully pretrain some initial policy

--- catkin_ws/src/journey/scripts/deep_drone.py
+++ catkin_ws/src/journey/scripts/deep_drone.py
@@ -29,15 +29,6 @@ RATE = 10  # Hz
 class DeepDronePlanner:
 
     def __init__(self):
-        # Actions.
-        options = [-1, 0, 1]
-        num_actions = 4
-        self.actions = np.zeros((len(options)**num_actions, num_actions))
-        for i in range(len(options)**num_actions):
-            temp = i
-            for j in range(num_actions):
-                self.actions[i][j] = options[temp % len(options)]
-                temp /= len(options)
 
         rospy.init_node('deep_drone_planner', anonymous=True)
         self.velocity_publisher = rospy.Publisher(
@@ -52,7 +43,7 @@ class DeepDronePlanner:
         s = rospy.Service('fly_to_goal', FlyToGoal, self.FlyToGoal)
         self.pose = Pose()
         self.rate = rospy.Rate(RATE)
-        self.image = None
+        self.image_msg = None
 
         # Initialize goal
         self.goal_pose = Pose()
@@ -60,23 +51,43 @@ class DeepDronePlanner:
         self.goal_pose.position.y = 0
         self.goal_pose.position.z = 1
 
-        (self.image_input, self.delta_input, self.reward_input,
-         self.control_output, self.optimizer) = self._CreateModel()
+        # Actions.
+        options = [-1, 0, 1]
+        num_actions = 4
+        self.actions = np.zeros((len(options)**num_actions, num_actions))
+        for i in range(len(options)**num_actions):
+            temp = i
+            for j in range(num_actions):
+                self.actions[i][j] = options[temp % len(options)]
+                temp /= len(options)
+
+        # Create model.
+        self._CreateModel()
+
+        # Start tensorflow session.
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
+
+        # Initialize policy.
+        self._InitializePolicy()
 
         print("Deep drone planner initialized.")
 
     def _CreateModel(self):
-        image = tf.placeholder(tf.float32, (None, 360, 640, 3), name='input')
-        delta = tf.placeholder(tf.float32, (None, 3), name='delta')
-        reward = tf.placeholder(tf.float32, (None), name='reward')
-        x = delta
-        x = tf.contrib.layers.fully_connected(x, 16)
-        logits = tf.contrib.layers.fully_connected(
-            x, len(self.actions), activation_fn=None)
-        outputs = tf.argmax(logits)
+        self.image = tf.placeholder(
+            tf.float32, (None, 360, 640, 3), name='input')
+        self.delta = tf.placeholder(tf.float32, (None, 3), name='delta')
+        self.actions = tf.placeholder(tf.float32, (None, 4), name='actions')
+        self.reward = tf.placeholder(tf.float32, (None), name='reward')
+        x = self.delta
+        x = tf.contrib.layers.fully_connected(x, 32)
+        x = tf.contrib.layers.fully_connected(x, 32)
+        x = tf.contrib.layers.fully_connected(x, 4, activation_fn=None)
+        self.controls = tf.clip_by_value(x, -1, 1)
 
         # Define the loss function
-        loss = tf.log(tf.nn.softmax(x)) * reward
+        self.loss = tf.reduce_mean(tf.abs(self.actions - x))
+        # loss = tf.log(tf.nn.softmax(x)) * reward
 
         # Adam will likely converge much faster than SGD for this assignment.
         optimizer = tf.train.AdamOptimizer(0.001, 0.9, 0.999)
@@ -85,9 +96,24 @@ class DeepDronePlanner:
         # batch_norm parameters are properly updated)
         with tf.control_dependencies(
                 tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
-            opt = optimizer.minimize(loss)
+            self.optimizer = optimizer.minimize(self.loss)
 
-        return (image, delta, reward, outputs, opt)
+    def _InitializePolicy(self):
+        num_samples = 1000
+        bounds = 5
+        tolerance = 0.5
+        delta = (np.random.uniform(size=(num_samples, 3)) - 0.5) * (2 * bounds)
+        actions = np.zeros((num_samples, 4))
+        actions[np.where(delta > tolerance)] = 1
+        actions[np.where(delta < -tolerance)] = -1
+        # print(delta)
+        # print(actions)
+        for epoch in range(300):
+            loss_val, _ = self.sess.run(
+                [self.loss, self.optimizer],
+                feed_dict={self.delta: delta,
+                           self.actions: actions})
+        print("Policy initialization loss: %s" % loss_val)
 
     def _OnNewPose(self, data):
         self.pose.position.x = round(data.x, 4)
@@ -104,40 +130,35 @@ class DeepDronePlanner:
         return FlyToGoalResponse(True)
 
     def _OnNewImage(self, image):
-        self.image = image
+        self.image_msg = image
 
-    def QueryPolicy(self, sess, image, delta):
+    def QueryPolicy(self, image, delta):
         # TODO(kirmani): Do on-policy learning here.
         image = np.stack([image])
         delta = np.stack([delta])
-        output = sess.run(
-            [self.control_output],
-            feed_dict={self.image_input: image,
-                       self.delta_input: delta})[0][0]
-        return self.actions[output]
+        controls = self.sess.run(
+            [self.controls], feed_dict={self.image: image,
+                                        self.delta: delta})[0][0]
+        return controls
 
-    def ImprovePolicy(self, sess, delta, reward):
+    def ImprovePolicy(self, delta, reward):
         # TODO(kirmani): Do policy optimization step.
         delta = np.stack([delta])
-        sess.run(
+        self.sess.run(
             [self.optimizer],
             feed_dict={self.delta_input: delta,
                        self.reward_input: reward})
 
     def Plan(self):
-        # Start tensorflow session.
-        sess = tf.Session()
-        sess.run(tf.global_variables_initializer())
-
         # Initialize velocity message.
         vel_msg = Twist()
 
         while not rospy.is_shutdown():
-            if not self.image:
+            if not self.image_msg:
                 print("No image available.")
             else:
                 # Create inputs for network.
-                input_image = ros_numpy.numpify(self.image)
+                input_image = ros_numpy.numpify(self.image_msg)
                 goal = np.array([
                     self.goal_pose.position.x, self.goal_pose.position.y,
                     self.goal_pose.position.z
@@ -149,7 +170,7 @@ class DeepDronePlanner:
                 input_delta = goal - x
 
                 # Output some control.
-                controls = self.QueryPolicy(sess, input_image, input_delta)
+                controls = self.QueryPolicy(input_image, input_delta)
                 print("Controls: %s" % controls)
                 vel_msg.linear.x = controls[0]
                 vel_msg.linear.y = controls[1]
@@ -171,18 +192,18 @@ class DeepDronePlanner:
                 reward = np.exp(-distance)
                 print("Reward: %s" % reward)
 
-                # Improve policy.
-                self.ImprovePolicy(sess, input_delta, reward)
+                # # Improve policy.
+                # self.ImprovePolicy(input_delta, reward)
 
-                # Start training on new goal if we succeed at this one.
-                if (reward > 0.95):
-                    print("Succeeded at reaching goal: %s" % goal)
-                    new_goal = np.random.uniform(size=3) * 3 + np.array(
-                        [0, 0, 1])
-                    self.goal_pose.position.x = new_goal[0]
-                    self.goal_pose.position.y = new_goal[1]
-                    self.goal_pose.position.z = new_goal[2]
-                    print("Going to new goal: %s" % new_goal)
+                # # Start training on new goal if we succeed at this one.
+                # if (reward > 0.95):
+                #     print("Succeeded at reaching goal: %s" % goal)
+                #     new_goal = np.random.uniform(size=3) * 3 + np.array(
+                #         [0, 0, 1])
+                #     self.goal_pose.position.x = new_goal[0]
+                #     self.goal_pose.position.y = new_goal[1]
+                #     self.goal_pose.position.z = new_goal[2]
+                #     print("Going to new goal: %s" % new_goal)
 
             # Wait.
             self.rate.sleep()
