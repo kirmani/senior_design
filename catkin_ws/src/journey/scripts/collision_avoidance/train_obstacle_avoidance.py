1508328845af606c8d780657e9e5e63b004b14a6
Sean Kirmani <sean@kirmani.io>
2017-12-13 22:46:34 -0600

    reset pose to start every 100 episodes in case we escape the world

--- catkin_ws/src/journey/scripts/collision_avoidance/train_obstacle_avoidance.py
+++ catkin_ws/src/journey/scripts/collision_avoidance/train_obstacle_avoidance.py
@@ -84,6 +84,10 @@ class DeepDronePlanner:
         # The rate which we publish commands.
         self.rate = rospy.Rate(self.rate)
 
+        # Reset count.
+        self.reset_count = 0
+        self.reset_limit = 100
+
         # Set up policy search network.
         self.action_dim = 2
         scale = 0.1
@@ -319,8 +323,15 @@ class DeepDronePlanner:
                       self.last_collision_pose.orientation.z,
                       self.last_collision_pose.orientation.w)
         _, _, yaw = transform.transformations.euler_from_quaternion(quaternion)
-        position = (position[0], position[1], 1)
-        quaternion = transform.transformations.quaternion_from_euler(0, 0, yaw)
+        if self.reset_count == 0:
+            position = (0, 0, 0)
+            quaternion = transform.transformations.quaternion_from_euler(
+                0, 0, 0)
+        else:
+            position = (position[0], position[1], 1)
+            quaternion = transform.transformations.quaternion_from_euler(
+                0, 0, yaw)
+        self.reset_count = (self.reset_count + 1) % self.reset_limit
 
         reset_pose = Pose()
         reset_pose.position.x = position[0]
