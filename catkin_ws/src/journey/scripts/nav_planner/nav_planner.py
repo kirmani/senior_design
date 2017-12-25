0548df25c7176ae50592cd5eb48f693bbf6ab8c5
Sean Kirmani <sean@kirmani.io>
2017-12-25 15:07:19 -0800

    Integrate ROS Service for navigation controller (#75)
    * add working nav controller
    
    * made left negative and right positive
    
    * set absolute height of nav goal for finer altitude control

--- catkin_ws/src/journey/scripts/nav_planner/nav_planner.py
+++ catkin_ws/src/journey/scripts/nav_planner/nav_planner.py
@@ -11,11 +11,12 @@ Navigation planner.
 
 import rospy
 import numpy as np
+import tf
+from geometry_msgs.msg import Point
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import Twist
-from journey.srv import FlyToGoal
+from nav_msgs.msg import Odometry
 from std_msgs.msg import Empty as EmptyMessage
-from tum_ardrone.msg import filter_state
 
 
 class NavigationPlannerNode:
@@ -29,8 +30,8 @@ class NavigationPlannerNode:
         self.rate = rospy.Rate(self.update_rate)
 
         # Pose subscriber.
-        self.pose_subscriber = rospy.Subscriber(
-            '/ardrone/predictedPose', filter_state, self.on_new_pose)
+        self.pose_subscriber = rospy.Subscriber('/ground_truth/state', Odometry,
+                                                self.on_new_pose)
         self.pose = Pose()
 
         # Actions.
@@ -42,12 +43,13 @@ class NavigationPlannerNode:
             '/ardrone/takeoff', EmptyMessage, queue_size=10)
 
         # Set navigation goal service.
-        self.set_nav_goal_service = rospy.Service('set_nav_goal', FlyToGoal, self.set_nav_goal)
+        self.set_nav_goal_service = rospy.Subscriber('/journey/set_nav_goal',
+                                                     Point, self.set_nav_goal)
         self.nav_goal = Pose()
-        self.nav_goal.position.z = 1.0
 
         # Take-off drone.
         rospy.sleep(2.0)
+        self.set_nav_goal(Point(0.0, 0.0, 1.0))
         self.takeoff_publisher.publish(EmptyMessage())
 
         print("Navigation planner initialzated.")
@@ -55,60 +57,50 @@ class NavigationPlannerNode:
         # Begin planning loop.
         self.planning_loop()
 
-    def on_new_pose(self, pose):
-        self.pose.position.x = pose.x
-        self.pose.position.y = pose.y
-        self.pose.position.z = pose.z
+    def on_new_pose(self, state):
+        self.pose = state.pose.pose
 
-    def set_nav_goal(self, nav_goal):
-        self.nav_goal.position.x = self.pose.position.x + nav_goal.dx
-        self.nav_goal.position.y = self.pose.position.y + nav_goal.dy
-        self.nav_goal.position.z = self.pose.position.z + nav_goal.dz
+    def set_nav_goal(self, nav_delta):
+        self.nav_goal.position.x = self.pose.position.x + nav_delta.x
+        self.nav_goal.position.y = self.pose.position.y + nav_delta.y
+        self.nav_goal.position.z = nav_delta.z
+        print("Set navigation goal: (%.4f, %.4f, %.4f)" %
+              (self.nav_goal.position.x, self.nav_goal.position.y,
+               self.nav_goal.position.z))
 
     def planning_loop(self):
-        # PID controller initialization.
-        error_prior = np.zeros(3)
-        integral = np.zeros(3)
-        k_i = np.zeros(3)
-        k_d = np.zeros(3)
-        p_u = 1.0
-        bias = np.zeros(3)
-
         while not rospy.is_shutdown():
             x = np.array([
-                    self.pose.position.x,
-                    self.pose.position.y,
-                    self.pose.position.z
-                ])
+                self.pose.position.x, self.pose.position.y, self.pose.position.z
+            ])
+            quaternion = (self.pose.orientation.x, self.pose.orientation.y,
+                          self.pose.orientation.z, self.pose.orientation.w)
+            _, _, yaw = tf.transformations.euler_from_quaternion(quaternion)
             g = np.array([
-                    self.nav_goal.position.x,
-                    self.nav_goal.position.y,
-                    self.nav_goal.position.z
-                    ])
-            error = (g - x)
+                self.nav_goal.position.x, self.nav_goal.position.y,
+                self.nav_goal.position.z
+            ])
 
-            # Zielger-Nichols method for PID tuning.
-            k_u = error
-
-            k_p = 0.6 * k_u
-            k_i = 2 * k_p / p_u
-            k_d = k_p * p_u / 8.0
-
-            integral = integral + error * (1.0 / self.update_rate)
-            derivative = (error - error_prior) * self.update_rate
-            output = (k_p * error + k_i * integral + k_d * derivative + bias)
-            error_prior = error
-
+            # Proportional controller.
             vel_msg = Twist()
-            vel_msg.linear.x = output[0]
-            vel_msg.linear.y = output[1]
-            vel_msg.linear.z = output[2]
-            vel_msg.angular.z = 0
+
+            if np.linalg.norm(g[:2] - x[:2]) > 0.5:
+                # Linear velocity in the forward axis.
+                vel_msg.linear.x = np.clip(0.2 * np.linalg.norm(g[:2] - x[:2]),
+                                           -1, 1)
+
+                # Angular velocity in the XY plane.
+                vel_msg.angular.z = np.clip(
+                    -1.0 * (np.arctan2(g[1] - x[1], g[0] - x[0]) - yaw), -1, 1)
+
+            # Linear velocity in the up axis.
+            vel_msg.linear.z = np.clip(0.2 * np.linalg.norm(g[2] - x[2]), -1, 1)
+
+            # Publish our velocity message.
             self.velocity_publisher.publish(vel_msg)
 
             # Wait.
             self.rate.sleep()
-
 
 
 def main():
