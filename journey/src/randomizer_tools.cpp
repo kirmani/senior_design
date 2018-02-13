721ad4ec9b2a4e3bbc229ea67673291a1f4a3097
Sean Kirmani <sean@kirmani.io>
2018-02-12 18:53:48 -0600

    Can publish to gazebo light topic from ROS

--- journey/src/randomizer_tools.cpp
+++ journey/src/randomizer_tools.cpp
@@ -1,94 +1,77 @@
 #include "ros/ros.h"
 #include "std_msgs/Empty.h"
 
-#include <random>
-
 #include <gazebo/gazebo.hh>
-#include <gazebo/transport/transport.hh>
+#include <gazebo/gazebo_client.hh>
+#include <gazebo/math/gzmath.hh>
 #include <gazebo/msgs/msgs.hh>
-#include <math/gzmath.hh>
+#include <gazebo/rendering/rendering.hh>
+#include <gazebo/transport/transport.hh>
+#include <gazebo/transport/transport.hh>
+#include <ignition/math/Pose3.hh>
 
 #include <iostream>
+#include <random>
 
-#include <transport/transport.hh>
-#include <rendering/rendering.hh>
-
-
-//TODO armand find some way to change the intensity of the light
+// TODO armand find some way to change the intensity of the light
 class RandomizerTools {
  public:
+  RandomizerTools(const gazebo::transport::NodePtr &node) {
+    // Publish to a Gazebo topic
+    pub_ = node->Advertise<gazebo::msgs::Light>("~/light/modify");
 
-  struct pose_t {
-    float x;
-    float y;
-    float z;
-    float roll;
-    float pitch;
-    float yaw;
-} ;
+    light_.set_name("sun");
 
-  RandomizerTools(ros::NodeHandle node) {
     std::cout << "Randomizer tool initialized." << std::endl;
-    
-    /*initialization code*/
-    // Create our node for communication
-    node_ = new gazebo::transport::Node();
-    node_->Init(
-
-    // Start transport
-    gazebo::transport::run();
   }
 
   void OnRandomize(const std_msgs::Empty::ConstPtr &msg) {
     std::cout << "OnRandomize received." << std::endl;
 
-    // Publish to a Gazebo topic
-    gazebo::transport::PublisherPtr pub = node_->Advertise<gazebo::msgs::Light>("~/light/modify");
-
     // Wait for a subscriber to connect
-    pub->WaitForConnection();
+    pub_->WaitForConnection();
 
-    // Throttle Publication (make sure gazebo and ros are like updating at the same time ish)
-    gazebo::common::Time::MSleep(100); //TODO what does this do?
+    ignition::math::Pose3d pose_sun = GetRandomPose();
+    gazebo::msgs::Set(light_.mutable_pose(), pose_sun);
 
-    msgs::Light msg;
-    msg.set_name("sun");
-
-    pose_t pose_sun;
-    pose_randomization(&pose_sun);
-    msgs::Set(msg.mutable_pose(), <0 5 5 pose_sun.roll pose_sun.pitch pose_sun.yaw>); 
-    //might have to use quaterniond and stuff
-    //type in gazebo world plugin tutorial to get to
-
-    pub->Publish(msg);
+    pub_->Publish(light_);
   }
 
   /*randomly chooses pose (we're just gonna do roll pitch and yaw*/
-  //TODO gazbo pose in radians or degrees? in tutorial it's in radians, so let's do radians
-  void pose_randomization(pose_t* pose) {
+  // TODO gazbo pose in radians or degrees? in tutorial it's in radians, so
+  // let's do radians
+  static ignition::math::Pose3d GetRandomPose() {
+    std::uniform_real_distribution<float> distribution(0.0, 1.0);
     std::default_random_engine generator;
-    std::uniform_real_distribution<float> distribution(0.0, 1.0); 
-    pose->roll = distribution(generator) * 0.1;
-    pose->pitch = distribution(generator) * 0.1;
-    pose->yaw = distribution(generator) * 0.1;    
-}    
+    return ignition::math::Pose3d(0, 0, 10, distribution(generator),
+                                  distribution(generator),
+                                  distribution(generator));
+  }
 
  private:
-  gazebo::transport::NodePtr node_;
+  gazebo::transport::PublisherPtr pub_;
+  gazebo::msgs::Light light_;
 };
 
 int main(int argc, char **argv) {
+  // Setup ROS>
   ros::init(argc, argv, "randomizer_tools");
-  ros::NodeHandle node;
+  ros::NodeHandle ros_node;
 
-  RandomizerTools randomizer_tools(node);
+  // Setup gazebo.
+  gazebo::client::setup(argc, argv);
+  gazebo::transport::NodePtr gazebo_node(new gazebo::transport::Node());
+  gazebo_node->Init();
+  gazebo::transport::run();
 
+  // Create our randomizer tools.
+  RandomizerTools randomizer_tools(gazebo_node);
+
+  // Subscribe to /journey/randomize.
   ros::Subscriber sub =
-      node.subscribe("/journey/randomize", 1000, &RandomizerTools::OnRandomize,
-                     &randomizer_tools);
+      ros_node.subscribe("/journey/randomize", 1000,
+                         &RandomizerTools::OnRandomize, &randomizer_tools);
 
   ros::spin();
   return 0;
 }
-
-
